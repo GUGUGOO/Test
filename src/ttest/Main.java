bd9d237ad93421f199b22e62947617304d5e522f
hyunggu <hyungboy12@gmail.com>
2019-06-03 10:45:15 +0900

    first commit

---
+++ src/ttest/Main.java
@@ -0,0 +1,15 @@
+package ttest;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		test t = new test();
+		
+		t.setName("¹ÚÇü±¸");
+		
+		t.show();
+		
+		System.out.println(t.getName());
+	}
+
+}
---
+++ src/ttest/test.java
@@ -0,0 +1,24 @@
+package ttest;
+
+public class test {
+	
+	private String name;
+	private int age;
+	
+	test(){
+		
+	}
+	
+	public void show() {
+		
+		System.out.println(this.name);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+}
